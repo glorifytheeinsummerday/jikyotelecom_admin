65d34f57c4f49dedda47f4930ad512ffb9879253
summerglory_alphatree <101625062+glorifytheeinsummerday@users.noreply.github.com>
2024-10-01 01:17:32 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // CORS 모듈 추가
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
 
-app.use(cors()); // CORS 허용
+// SSL 인증서 및 키 파일 경로 설정
+// Let's Encrypt를 사용하여 발급받은 인증서와 키 파일의 경로입니다.
+// 도메인에 맞게 경로를 수정하세요.
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/www.jikyotelecom.com/privkey.pem'),  // 개인 키 파일 경로
+    cert: fs.readFileSync('/etc/letsencrypt/live/www.jikyotelecom.com/fullchain.pem') // 인증서 파일 경로
+};
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
 
+// 간단한 로그인 엔드포인트
 app.post('/admin-dashboard', (req, res) => {
     const { username, password } = req.body;
 
-    // 추가된 아이디와 비밀번호
+    // 허용된 사용자 목록
     const validUsers = [
         { username: 'admin', password: 'admin123' },
         { username: 'admin1', password: 'password1' },
         { username: 'admin2', password: 'password2' }
     ];
 
-    // 로그인 검증
+    // 사용자 인증
     const user = validUsers.find(user => user.username === username && user.password === password);
     if (user) {
-        res.json({ success: true }); // 로그인 성공 시 JSON 응답
+        res.json({ success: true });
     } else {
         res.json({ success: false, message: '잘못된 사용자 이름 또는 비밀번호' });
     }
 });
 
-// '0.0.0.0'을 사용하여 모든 네트워크 인터페이스에서 요청 수신
-app.listen(port, '0.0.0.0', () => {
-    console.log(`서버가 https://185.199.108.153:${port}에서 실행 중입니다.`);
+// HTTPS 서버 실행
+https.createServer(options, app).listen(port, () => {
+    console.log(`서버가 https://www.jikyotelecom.com:${port}에서 실행 중입니다.`);
 });
